d929722bb72f9a4ca81e643b58558e81ad0552e0
CABALLERO BARANDICA LUIS <lcaballl@cuc.edu.co>
2022-03-02 19:38:39 -0500

    primer punto

--- 5.py
+++ 5.py
@@ -1,4 +1,5 @@
-def primerPunto(monto):
+def primerPunto():
+    monto = int(input("Ingrese donacion"))
     telecomunicacione = monto * 0.1
     sistemas = monto * 0.25
     administracion = monto * 0.35
@@ -6,4 +7,4 @@ def primerPunto(monto):
     print(f"La donacion fue de: {monto}  y se repartiran de la siguiente forma: \nTelecomunicaciones = {telecomunicacione} \nSistemas = {sistemas} \nAdministracion = {administracion} \nContabilidad = {contabilidad}")
 
 
-primerPunto(5000000)
+primerPunto()
